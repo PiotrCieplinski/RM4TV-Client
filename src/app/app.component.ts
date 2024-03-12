0e971eb5980f2a8f9becfc81bf38d11bfb4349d8
Piotr Ciepliński <piotr.cieplinski@polsl.pl>
2024-03-12 12:18:13 +0100

    poprawki pod telewizor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ import { ContentObserver } from '@angular/cdk/observers';
   standalone: true,
   imports: [RouterOutlet, HttpClientModule, CommonModule, MatTableModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   protected readonly API_Path = environment.apiUrl;
@@ -26,28 +26,39 @@ export class AppComponent {
   public displayedColumns = ['hours', 'name', 'teacher', 'room', 'group'];
   private interval: any;
 
-  protected schedulesData$ = this.httpClient.get<APISchedulesResponse>(`${this.API_Path}/schedules`).pipe(delayWhen(x => {
-    const currentInterval = this.nextSchedulesInterval;
-    this.nextSchedulesInterval = Math.max(x.items.length * 1250, 10000);
-    return interval(currentInterval);
-  }), repeat());
+  protected schedulesData$ = this.httpClient
+    .get<APISchedulesResponse>(`${this.API_Path}/schedules`)
+    .pipe(
+      delayWhen((x) => {
+        const currentInterval = this.nextSchedulesInterval;
+        this.nextSchedulesInterval = Math.max(x.items.length * 1250, 10000);
+        return interval(currentInterval);
+      }),
+      repeat()
+    );
 
-  protected screensData$ = this.httpClient.get<APIScreensResponse>(`${this.API_Path}/screens`).pipe(delayWhen(x => {
-    if (this.firstScreensRes) {
-      this.firstScreensRes = false;
-      this.lastItemWasImage = x.isImage;
-      return interval(0);
-    }
+  protected screensData$ = this.httpClient
+    .get<APIScreensResponse>(`${this.API_Path}/screens`)
+    .pipe(
+      delayWhen((x) => {
+        if (this.firstScreensRes) {
+          this.firstScreensRes = false;
+          this.lastItemWasImage = x.isImage;
+          return interval(0);
+        }
 
-    const res = this.lastItemWasImage ? interval(10000) : interval(120000);
-    this.lastItemWasImage = x.isImage;
-    return res;
-  }), repeat());
+        const res = this.lastItemWasImage ? interval(10000) : interval(120000);
+        this.lastItemWasImage = x.isImage;
+        return res;
+      }),
+      repeat()
+    );
 
   ngAfterViewInit(): void {
     const contentWrapper = document.querySelector('.schedule-container');
     if (contentWrapper) {
-      this.contentObserver.observe(contentWrapper)
+      this.contentObserver
+        .observe(contentWrapper)
         .subscribe(this.tableContentChanged);
     }
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost/RM4API'
+  production: false,
+  apiUrl: 'http://localhost/RM4API',
 };
